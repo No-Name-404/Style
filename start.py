574cef00f163d3610ae536bb81fa7201d2a5a349
root <root@kali.kali>
2020-02-09 21:21:31 +0300

    Style2

--- start.py
+++ start.py
@@ -9,15 +9,15 @@ except ModuleNotFoundError:
 
 class app:
     def run(self):
-        self.Introduction()
+        self.Introduction() # Intro ...
         user = int(input(Text('G#[W#*G#] Enter numder : W#')))
         self.StartApp(user)
 
-    def Introduction(self):
+    def Introduction(self): # Intro ...
         os.system('clear')
         Intro = str( Text(db.Intro) )
         Intro = Text(Intro).DeleteSpace
-        title = Style('### Style {1.0} ###')
+        title = Style('### Style B#{G#2.0B#}W# ###')
         title = title.Square(padding_x=12)
         tools = ['[G#1W#]C# Python','[G#2W#]C# Bash ']
         tools = Style(*tools)
@@ -27,19 +27,29 @@ class app:
         text = Style(Intro,title,tools,end).Center
         Animation.SlowLine(text,time=0.0001)
 
-    def StyleText(self):
+    def StyleText(self): # Style system...
         color = self.RandColor()
         cols = int(input(Text('G#[W#*G#] cols : W#')))
         padding = int(input(Text('G#[W#*G#] padding : W#')))
+        space = int(input(Text('G#[W#*G#] spacing : W#')))
+        In = input(Text('G#[W#*G#] Intro {false/text} : W#'))
         title = input(Text('G#[W#*G#] title : W#'))
         tools = input(Text('G#[W#*G#] tools : W#'))
         end = input(Text('G#[W#*G#] end : W#'))
+        if In.lower() == 'false':
+            Intro = random.choice([color[3]+db.dragon.replace('8',color[3]+'8'+color[2]),W+db.skull])
+        else:
+            Intro = os.popen(f'figlet {In}').read()
+            Intro = Text(Intro).DeleteSpace
+            Intro = W+Intro
 
-        Intro = random.choice([color[3]+db.dragon.replace('8',color[3]+'8'+color[2]),W+db.skull])
         title = Style( str(Text(title)) )
         title = title.Square(Color=color[0])
         tools = [f'{color[3]}[{color[2]}{N+1}{color[3]}] {T}' for N,T in enumerate(str(Text(tools)).split(' '))]
-        tools = Style(*tools).Square(Color=color[1],padding_x=padding,cols=cols)
+        tools = Style(*tools).Square(Color=color[1],
+                                    padding_x=padding,
+                                    cols=cols,
+                                    Space=space)
         end = Style(end).Square(Color=color[0])
         text = Style(Intro,title,tools,end).Center
         return text
@@ -77,63 +87,54 @@ class app:
         pyORba[user]()
 
     def python(self):
-        os.system('clear')
-        Intro = str( Text(db.Intro) )
-        Intro = Text(Intro).DeleteSpace
-        title = Style('### Style {1.0} ###')
-        title = title.Square(padding_x=12)
-        end = Style('python ')
-        end = end.Square(padding_x=18)
-        text = Style(Intro,title,end).Center
-        Animation.SlowLine(text,time=0.0001)
-        Style_Text = self.StyleText()
+        self.IntroS2('python ') # Intro Step 2 ...
+        Style_Text = self.StyleText() # Style system...
         file = input(Text('G#[W#*G#] file :W# '))
         print ('')
         Animation.Loading(text='G#[W#*G#]W# Loading ')
         print (Text(f'G#[W#*G#] Saved [C#SERVICES/{file}G#]'))
         Style_Text = Style_Text.replace('\\','\\\\')
-        Style_Text = Style_Text.replace('\033[1;30m','\\033[1;30m')
-        Style_Text = Style_Text.replace('\033[0;31m','\\033[0;31m')
-        Style_Text = Style_Text.replace('\033[0;32m','\\033[0;32m')
-        Style_Text = Style_Text.replace('\033[0;33m','\\033[0;33m')
-        Style_Text = Style_Text.replace('\033[0;34m','\\033[0;34m')
-        Style_Text = Style_Text.replace('\033[0;35m','\\033[0;35m')
-        Style_Text = Style_Text.replace('\033[0;36m','\\033[0;36m')
-        Style_Text = Style_Text.replace('\033[0;37m','\\033[0;37m')
+        Style_Text = self.FileColor(Style_Text) # Color Files ...
         Style_Text = Style_Text.replace('\n','\\n')
         with open('SERVICES/'+file,'w') as f:
             f.write(f"#!/usr/bin/python\n# - * - coding: utf-8 - * -\ndef Style():\n\tfrom V7xStyle import Animation\n\ttext = '''{Style_Text}'''\n\tAnimation.SlowLine(text,time=0.0001)\nStyle()")
 
     def bash(self):
-        os.system('clear')
-        Intro = str( Text(db.Intro) )
-        Intro = Text(Intro).DeleteSpace
-        title = Style('### Style {1.0} ###')
-        title = title.Square(padding_x=12)
-        end = Style(' bash  ')
-        end = end.Square(padding_x=18)
-        text = Style(Intro,title,end).Center
-        Animation.SlowLine(text,time=0.0001)
-        Style_Text = self.StyleText()
+        self.IntroS2(' bash  ') # Intro Step 2 ...
+        Style_Text = self.StyleText() # Style system...
         file = input(Text('G#[W#*G#] file :W# '))
         print ('')
         Animation.Loading(text='G#[W#*G#]W# Loading ')
         print (Text(f'G#[W#*G#] Saved [C#SERVICES/{file}G#]'))
         Style_Text = Style_Text.replace('\\','\\\\')
-        Style_Text = Style_Text.replace('\033[1;30m','\\033[1;30m')
-        Style_Text = Style_Text.replace('\033[0;31m','\\033[0;31m')
-        Style_Text = Style_Text.replace('\033[0;32m','\\033[0;32m')
-        Style_Text = Style_Text.replace('\033[0;33m','\\033[0;33m')
-        Style_Text = Style_Text.replace('\033[0;34m','\\033[0;34m')
-        Style_Text = Style_Text.replace('\033[0;35m','\\033[0;35m')
-        Style_Text = Style_Text.replace('\033[0;36m','\\033[0;36m')
-        Style_Text = Style_Text.replace('\033[0;37m','\\033[0;37m')
+        Style_Text = self.FileColor(Style_Text) # Color Files ...
         Style_Text = Style_Text.replace('`','\\`')
         Style_Text = Style_Text.replace('"','\\"')
         with open('SERVICES/'+file,'w') as f:
             for i in Style_Text.split('\n'):
                 f.write(f'echo -e "{i} "\nsleep 0.01\n')
 
+    def FileColor(self,Style_Text): # Color Files ...
+        Style_Text = Style_Text.replace('\033[1;30m','\\033[1;30m')
+        Style_Text = Style_Text.replace('\033[0;31m','\\033[0;31m')
+        Style_Text = Style_Text.replace('\033[0;32m','\\033[0;32m')
+        Style_Text = Style_Text.replace('\033[0;33m','\\033[0;33m')
+        Style_Text = Style_Text.replace('\033[0;34m','\\033[0;34m')
+        Style_Text = Style_Text.replace('\033[0;35m','\\033[0;35m')
+        Style_Text = Style_Text.replace('\033[0;36m','\\033[0;36m')
+        Style_Text = Style_Text.replace('\033[0;37m','\\033[0;37m')
+        return Style_Text
+
+    def IntroS2(self,tool): # Intro Step 2 ...
+        os.system('clear')
+        Intro = str( Text(db.Intro) )
+        Intro = Text(Intro).DeleteSpace
+        title = Style('### Style B#{G#2.0B#}W# ###')
+        title = title.Square(padding_x=12)
+        end = Style(tool)
+        end = end.Square(padding_x=18)
+        text = Style(Intro,title,end).Center
+        Animation.SlowLine(text,time=0.0001)
 
 if __name__ == '__main__':
     app = app()
